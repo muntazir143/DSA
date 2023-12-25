778f3d3d8f9cf6be62094dac684a4ff5bbff4b1a
muntazir143 <50302549+muntazir143@users.noreply.github.com>
2023-12-25 11:10:50 +0000

    factorial addition

--- recursion.js
+++ recursion.js
@@ -8,3 +8,12 @@ function power(num, pow) {
 }
 
 console.log(power(2, 4));
+
+function factorial(num) {
+  if (num == 0 || num == 1) {
+    return 1;
+  }
+  return num * factorial(num - 1);
+}
+
+console.log(factorial(4));
